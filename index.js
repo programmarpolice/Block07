199720c7bce3e5a08d7fdf8006f7213bcf169a7b
Joy Harrington <joysyk23@gmail.com>
2024-09-04 16:10:31 -0500

    Finished Javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+// I need to assign three calculations to variables
+// I need the results of those calculations to equal 10, 40, 39
+// I need to create the message "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+// I need to create an alert/dialog box that displays the vault code and the text
+
+
+// Step One - Create three calculations to equal vault code 10, 40, 30
+// Calculation one:
+const oneCode = 5 + 5;
+
+//Calculation two:
+const twoCode = 20 * 2;
+
+// Calcuation three:
+const threeCode = 40 - 1;
+
+// Step Two - Create the display message
+const impMessage = ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:" )
+
+// Step Three - Create dialog popup
+alert (impMessage + " " + oneCode + "," + twoCode + "," + threeCode)
+
+
+// Display message about workshop
+console.log ("This is my  Block 7 workshop")
